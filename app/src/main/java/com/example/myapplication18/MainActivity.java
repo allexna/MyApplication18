71e5d83606dd4800ce3f2698e894c49daa0bc719
allexna <alexna39@gmail.com>
2020-01-29 12:19:26 +0200

    29

--- app/src/main/java/com/example/myapplication18/MainActivity.java
+++ app/src/main/java/com/example/myapplication18/MainActivity.java
@@ -7,10 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 import android.widget.TextView;
 import android.view.MenuItem;
-
+import android.support.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +22,40 @@ public class MainActivity extends AppCompatActivity {
         Button button=findViewById(R.id.button);
         TextView textView=findViewById(R.id.textView);
         ImageView imageView=findViewById(R.id.imageView);
+        button.setOnClickListener(viewClickListener);
+        textView.setOnClickListener(viewClickListener);
+        imageView.setOnClickListener(viewClickListener);
     }
-    public void onClick(View view){
+    View.OnClickListener viewClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showPopupmenu(v);
 
+        }
+    };
+    private void showPopupmenu(View view){
+        PopupMenu popupMenu=new PopupMenu(this,view);
+        popupMenu.inflate(R.menu.popupmenu);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu1:
+                        Toast.makeText(getApplicationContext(), "вы выбрали меню 1", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menu2:
+                        Toast.makeText(getApplicationContext(), "вы выбрали меню 2", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menu3:
+                        Toast.makeText(getApplicationContext(), "вы выбрали меню 3", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default: return false;
+                }
+
+            }
+        });
     }
+    //public void onClick(View view){
+
+    //}
 }
